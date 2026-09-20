547c6fd9bdf08bfe5c7f40fcb74070f65d58bf86
Firefly <3100587120+3100587120@users.noreply.github.com>
2026-09-20 14:37:11 +0800

    fix: use valid Keychain account attributes

--- client-p2p/src/local_vault_apple.mm
+++ client-p2p/src/local_vault_apple.mm
@@ -6,9 +6,11 @@ QByteArray p2pAppleLoadOrCreateVaultKey(const QString& keyName)
 {
     const auto service = QStringLiteral("org.p2pmessenger.local-vault").toUtf8();
     const auto account = keyName.toUtf8();
+    NSString* serviceName = [NSString stringWithUTF8String:service.constData()];
+    NSString* accountName = [NSString stringWithUTF8String:account.constData()];
     NSDictionary* query = @{(__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
-                            (__bridge id)kSecAttrService: [NSData dataWithBytes: service.constData() length: service.size()],
-                            (__bridge id)kSecAttrAccount: [NSData dataWithBytes: account.constData() length: account.size()],
+                            (__bridge id)kSecAttrService: serviceName,
+                            (__bridge id)kSecAttrAccount: accountName,
                             (__bridge id)kSecReturnData: @YES};
     CFTypeRef value = nullptr;
     if (SecItemCopyMatching((__bridge CFDictionaryRef)query, &value) == errSecSuccess) {
@@ -20,8 +22,8 @@ QByteArray p2pAppleLoadOrCreateVaultKey(const QString& keyName)
     if (SecRandomCopyBytes(kSecRandomDefault, key.size(), reinterpret_cast<uint8_t*>(key.data())) != errSecSuccess)
         return {};
     NSDictionary* item = @{(__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
-                           (__bridge id)kSecAttrService: [NSData dataWithBytes: service.constData() length: service.size()],
-                           (__bridge id)kSecAttrAccount: [NSData dataWithBytes: account.constData() length: account.size()],
+                           (__bridge id)kSecAttrService: serviceName,
+                           (__bridge id)kSecAttrAccount: accountName,
                            (__bridge id)kSecValueData: [NSData dataWithBytes: key.constData() length: key.size()],
                            (__bridge id)kSecAttrAccessible: (__bridge id)kSecAttrAccessibleWhenUnlockedThisDeviceOnly};
     return SecItemAdd((__bridge CFDictionaryRef)item, nullptr) == errSecSuccess ? key : QByteArray {};
